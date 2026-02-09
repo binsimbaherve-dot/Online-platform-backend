548e51a4b9c04ccc5de66205d4ab5393d4cd2c25
HerveTheLion <hervebinsimba@gmail.com>
2026-02-09 19:10:18 +0200

    Add public business homepage (Spring Boot static index)

---
+++ backend/backend/src/main/resources/static/js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+document.getElementById("cloudPlatformBtn").addEventListener("click", function () {
+  alert("Cloud Platform (member login) will be linked later.");
+});
